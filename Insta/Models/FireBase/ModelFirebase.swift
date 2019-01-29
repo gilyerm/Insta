f54a70cc27b927d9703a98895ab3c9c7f9b731cc
gilyerm <gilyerm@gmail.com>
2019-01-29 22:48:47 +0200

    fix problem
    sorry

--- Insta/Models/FireBase/ModelFirebase.swift
+++ Insta/Models/FireBase/ModelFirebase.swift
@@ -14,18 +14,26 @@ import FirebaseAuth
 import UIKit
 
 class ModelFirebase{
-    static let ref: DatabaseReference! = Database.database().reference()
-    static var storageRef :StorageReference = Storage.storage().reference()
-    static var auth : Auth = Auth.auth();
-    static let userref : DatabaseReference = ref.child("users")
-    static let postref : DatabaseReference = ref.child("posts")
-    static let followingref : DatabaseReference = ref.child("followings")
-    static let commentref : DatabaseReference = ref.child("comments")
-    static let likeref : DatabaseReference = ref.child("likes")
-    static let tagref : DatabaseReference = ref.child("tags")
+    let ref: DatabaseReference!
+    lazy var storageRef :StorageReference = Storage.storage().reference()
+    lazy var auth : Auth = Auth.auth();
+    let userref : DatabaseReference;
+    let postref : DatabaseReference;
+    let followingref : DatabaseReference;
+    let commentref : DatabaseReference;
+    let likeref : DatabaseReference;
+    let tagref : DatabaseReference;
     
     init() {
         FirebaseApp.configure()
+        ref = Database.database().reference()
+        userref = ref.child("users")
+        postref = ref.child("posts")
+        followingref = ref.child("followings")
+        commentref = ref.child("comments")
+        likeref = ref.child("likes")
+        tagref = ref.child("tags")
+
     }
 
 }
