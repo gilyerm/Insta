0b5b1ad44b041c338fd85eb135f421967db9e529
Unknown <zehavitjovani@cs.colman.ac.il>
2019-01-29 15:03:01 +0200

    building sign in and up - vid1

---
+++ signInVC.swift
@@ -0,0 +1,34 @@
+//
+//  signInVC.swift
+//  Insta
+//
+//  Created by admin on 29/01/2019.
+//  Copyright © 2019 Gil Yermiyah. All rights reserved.
+//
+
+import UIKit
+
+class signInVC: UIViewController {
+
+    //textFields
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    //buttons
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var forgotBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func signInBtn_click(_ sender: Any) {
+        print("sign in pressed")
+    }
+    
+    @IBAction func signUpBtn_click(_ sender: Any) {
+        print("sign up pressed")
+    }
+    
+}
