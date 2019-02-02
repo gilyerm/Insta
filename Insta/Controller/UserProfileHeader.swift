a45ac9ae76ae991bd469b41ac249a65fea5c2da1
Unknown <zehavitjovani@cs.colman.ac.il>
2019-02-02 11:43:26 +0200

    work for now

---
+++ Insta/Controller/UserProfileHeader.swift
@@ -0,0 +1,26 @@
+//
+//  UserProfileHeader.swift
+//  Insta
+//
+//  Created by admin on 02/02/2019.
+//  Copyright © 2019 Gil Yermiyah. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileHeader: UICollectionViewCell {
+    let profileImageView: UIImage = {
+        let iv = UIImageView
+        
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
--- Insta/Controller/UserProfileVC.swift
+++ Insta/Controller/UserProfileVC.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
-
+import Firebase
+ 
 private let reuseIdentifier = "Cell"
-
-class UserProfileVC: UICollectionViewController {
+private let headerIdentifier = "UserProfileHeader"
+ 
+ 
+class UserProfileVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,25 +23,22 @@ class UserProfileVC: UICollectionViewController {
 
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+        
+        
+        
+        self.collectionView?.backgroundColor = .white
+        //fetch user data
+        fetchCurrentUserData()
 
-        // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
@@ -46,44 +46,30 @@ class UserProfileVC: UICollectionViewController {
         // #warning Incomplete implementation, return the number of items
         return 0
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as? UserProfileHeader
+        return header!
+    }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    // MARK: API
     
+    func fetchCurrentUserData(){
+        guard let currentId = Auth.auth().currentUser?.uid else {return}
+        print("Current User ID is: \(currentId)")
+        Database.database().reference().child("users").child(currentId).child("username").observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let username = snapshot.value as? String else {return} ////MAKE IT WORK  !!!!!!!!
+        self.navigationItem.title = username
+        })
     }
-    */
-
-}
+ }
